f9df419c5b5d48cd3d2dd5b5f80a89e46d021168
Ivan Arsenev <iv.arsenev@gmail.com>
2024-09-16 19:17:34 -0300

    daisyui added

--- src/routes/news.tsx
+++ src/routes/news.tsx
@@ -5,5 +5,26 @@ export const Route = createFileRoute("/news")({
 })
 
 function News() {
-  return <div className="flex flex-col bg-slate-400 h-28">News page</div>
+  const counter = 10
+  return (
+    <div className="flex flex-col h-28">
+      <div className="flex-1 p-4 text-center">
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
